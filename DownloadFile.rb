7fc1eeb9fbab37eb38d678e03a76f180915481d2
sandeepks2255 <sandeepks2255@gmail.com>
2017-09-12 10:08:00 +0530

    program to download CSV file

--- DownloadFile.rb
+++ DownloadFile.rb
@@ -1,26 +1,26 @@
-# require 'csv'
-# require 'rubygems'
-# require 'zip'
-# require 'net/http'
+require 'csv'
+require 'rubygems'
+require 'zip'
+require 'net/http'
 
-# class DownloadFile
+class DownloadFile
 
-#     def initialize(url)
-#         @url = url
-#     end
+    def initialize(url)
+        @url = url
+    end
 
-#     def fetch
-#         Net::HTTP.start("s3.amazonaws.com") { |http|
-#             puts"downloading"
-#             content = http.get(@url)
-#             File.open("zipfile", "w"){ |file|
-#                 file.write(content.body)
-#                 puts"DOWNLOADED"
-#             }
-#         }
-#     end
-# end
+    def fetch
+        Net::HTTP.start("s3.amazonaws.com") { |http|
+            puts"downloading"
+            content = http.get(@url)
+            File.open("zipfile", "w"){ |file|
+                file.write(content.body)
+                puts"DOWNLOADED"
+            }
+        }
+    end
+end
 
-# @download_file=DownloadFile.new("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip")
-# @download_file.fetch
+@download_file=DownloadFile.new("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip")
+@download_file.fetch
 
